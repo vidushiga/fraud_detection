5d9bc3e053dd10f776e05f62888d48ad044e2518
vidushiga <vidushigangwar1@gmail.com>
2022-11-01 18:04:58 +0530

    added 3 new model code

--- model.py
+++ model.py
@@ -3,3 +3,12 @@
 print("adding all ml related code here")
 
 
+def model1():
+    print("adding code for ml model1")
+
+def model2():
+    print("adding code for ml model2")
+
+def model3():
+    print("adding code for model3")
+
